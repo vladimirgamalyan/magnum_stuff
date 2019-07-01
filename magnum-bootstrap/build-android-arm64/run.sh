1a637bd747a8c38de41d0494d885ae6552c02b04
Vladimir Gamalyan <vladimir.gamalyan@gmail.com>
2019-07-02 04:59:00 +0700

    Add magnum-bootstrap sample app build scripts

---
+++ magnum-bootstrap/build-android-arm64/run.sh
@@ -0,0 +1,9 @@
+cmake .. \
+    -DCMAKE_MODULE_PATH=/home/user/toolchains/modules \
+    -DCMAKE_ANDROID_NDK=/opt/android-ndk \
+    -DCMAKE_SYSTEM_NAME=Android \
+    -DCMAKE_SYSTEM_VERSION=28 \
+    -DCMAKE_ANDROID_ARCH_ABI=arm64-v8a \
+    -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang \
+    -DCMAKE_ANDROID_STL_TYPE=c++_static \
+    -DCMAKE_BUILD_TYPE=Release
