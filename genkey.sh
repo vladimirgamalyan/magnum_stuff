1ba9b02cd1be1046cbb29a3fd7d2f3d87804984d
Vladimir Gamalyan <vladimir.gamalyan@gmail.com>
2019-07-02 06:04:32 +0700

    Update genkey.sh

--- genkey.sh
+++ genkey.sh
@@ -1,3 +1,6 @@
-keytool -genkeypair -keystore $HOME/.android/debug.keystore \
+# keytool -genkeypair -keystore $HOME/.android/debug.keystore \
+#    -storepass android -alias androiddebugkey -keypass android \
+#    -keyalg RSA -validity 10000 -dname CN=,OU=,O=,L=,S=,C=
+keytool -genkey -v   -keystore $HOME/.android/debug.keystore \
     -storepass android -alias androiddebugkey -keypass android \
     -keyalg RSA -validity 10000 -dname CN=,OU=,O=,L=,S=,C=
