e0afb31ccf3bdee0b663754c2ddd2b91c5bc3f49
Vladimir Gamalyan <vladimir.gamalyan@gmail.com>
2019-07-02 09:03:00 +0700

    Set minSdkVersion to 21

--- corrade/build-android-arm/run.sh
+++ corrade/build-android-arm/run.sh
@@ -1,10 +1,10 @@
 cmake .. \
     -DCMAKE_ANDROID_NDK=/opt/android-ndk \
     -DCMAKE_SYSTEM_NAME=Android \
-    -DCMAKE_SYSTEM_VERSION=28 \
+    -DCMAKE_SYSTEM_VERSION=21 \
     -DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a \
     -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang \
     -DCMAKE_ANDROID_STL_TYPE=c++_static \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=/opt/android-ndk/platforms/android-28/arch-arm/usr \
+    -DCMAKE_INSTALL_PREFIX=/opt/android-ndk/platforms/android-21/arch-arm/usr \
     -DCORRADE_INCLUDE_INSTALL_PREFIX=/opt/android-ndk/sysroot/usr
--- magnum/build-android-arm/run.sh
+++ magnum/build-android-arm/run.sh
@@ -1,12 +1,12 @@
 cmake .. \
     -DCMAKE_ANDROID_NDK=/opt/android-ndk \
     -DCMAKE_SYSTEM_NAME=Android \
-    -DCMAKE_SYSTEM_VERSION=28 \
+    -DCMAKE_SYSTEM_VERSION=21 \
     -DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a \
     -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang \
     -DCMAKE_ANDROID_STL_TYPE=c++_static \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=/opt/android-ndk/platforms/android-28/arch-arm/usr \
+    -DCMAKE_INSTALL_PREFIX=/opt/android-ndk/platforms/android-21/arch-arm/usr \
     -DMAGNUM_INCLUDE_INSTALL_PREFIX=/opt/android-ndk/sysroot/usr \
 	-DTARGET_GLES2=ON \
 	-DWITH_ANDROIDAPPLICATION=ON
