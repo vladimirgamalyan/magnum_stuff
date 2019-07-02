6a3d97b2830fbbdb8775ad51e3af961d2958132f
Vladimir Gamalyan <vladimir.gamalyan@gmail.com>
2019-07-02 09:14:27 +0700

    Change midSdkVersion for application

--- magnum-bootstrap/build-android-arm64/run.sh
+++ magnum-bootstrap/build-android-arm64/run.sh
@@ -2,7 +2,7 @@ cmake .. \
     -DCMAKE_MODULE_PATH=/home/user/toolchains/modules \
     -DCMAKE_ANDROID_NDK=/opt/android-ndk \
     -DCMAKE_SYSTEM_NAME=Android \
-    -DCMAKE_SYSTEM_VERSION=28 \
+    -DCMAKE_SYSTEM_VERSION=21 \
     -DCMAKE_ANDROID_ARCH_ABI=arm64-v8a \
     -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang \
     -DCMAKE_ANDROID_STL_TYPE=c++_static \
