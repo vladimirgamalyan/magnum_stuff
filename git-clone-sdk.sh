3a516bb5d939a9c38ba6d9211e28a6134b401779
Vladimir Gamalyan <vladimir.gamalyan@gmail.com>
2019-07-04 11:30:33 +0700

    Add Android SDK git clone script

---
+++ git-clone-sdk.sh
@@ -0,0 +1,7 @@
+git clone https://aur.archlinux.org/android-sdk.git
+git clone https://aur.archlinux.org/android-sdk-build-tools.git
+git clone https://aur.archlinux.org/android-sdk-platform-tools.git
+git clone https://aur.archlinux.org/android-platform.git
+git clone git://github.com/mosra/toolchains
+git clone --single-branch --branch base-android git://github.com/mosra/magnum-bootstrap.git
+git clone git://github.com/vladimirgamalyam/magnum_stuff.git
