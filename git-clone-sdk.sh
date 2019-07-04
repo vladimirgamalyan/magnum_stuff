740d1492d9301c5feb216d34dfe16e38aab3efb7
Vladimir Gamalyan <vladimir.gamalyan@gmail.com>
2019-07-04 11:31:01 +0700

    Update git-clone-sdk.sh

--- git-clone-sdk.sh
+++ git-clone-sdk.sh
@@ -4,4 +4,3 @@ git clone https://aur.archlinux.org/android-sdk-platform-tools.git
 git clone https://aur.archlinux.org/android-platform.git
 git clone git://github.com/mosra/toolchains
 git clone --single-branch --branch base-android git://github.com/mosra/magnum-bootstrap.git
-git clone git://github.com/vladimirgamalyam/magnum_stuff.git
