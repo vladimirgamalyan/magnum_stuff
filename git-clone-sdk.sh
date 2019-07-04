9f7cd105d00f8714261a57dd40e586350371f7b7
Vladimir Gamalyan <vladimir.gamalyan@gmail.com>
2019-07-04 12:41:00 +0700

    Update git-clone-sdk.sh, update CMakeLists.txt

--- git-clone-sdk.sh
+++ git-clone-sdk.sh
@@ -2,5 +2,10 @@ git clone https://aur.archlinux.org/android-sdk.git
 git clone https://aur.archlinux.org/android-sdk-build-tools.git
 git clone https://aur.archlinux.org/android-sdk-platform-tools.git
 git clone https://aur.archlinux.org/android-platform.git
+git clone https://aur.archlinux.org/android-sdk-cmake.git
+
+git clone git://github.com/mosra/corrade.git
+git clone git://github.com/mosra/magnum
+
 git clone git://github.com/mosra/toolchains
 git clone --single-branch --branch base-android git://github.com/mosra/magnum-bootstrap.git
