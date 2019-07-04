42a000122545a1c73e03a7dc1abb92cb18ad6e44
Vladimir Gamalyan <vladimir.gamalyan@gmail.com>
2019-07-04 12:54:08 +0700

    Add git-clone-bootstrap-project.sh

---
+++ git-clone-bootstrap-project.sh
@@ -0,0 +1,2 @@
+git clone git://github.com/mosra/toolchains
+git clone --single-branch --branch base-android git://github.com/mosra/magnum-bootstrap.git
--- git-clone-sdk.sh
+++ git-clone-sdk.sh
@@ -6,6 +6,3 @@ git clone https://aur.archlinux.org/android-sdk-cmake.git
 
 git clone git://github.com/mosra/corrade.git
 git clone git://github.com/mosra/magnum
-
-git clone git://github.com/mosra/toolchains
-git clone --single-branch --branch base-android git://github.com/mosra/magnum-bootstrap.git
